fb17587bfb3a8d0716500497cd6b19ef04b3600b
alimirmohammad <ali.mirmohammad.86@gmail.com>
2021-04-08 12:53:19 +0430

    start ref

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
+import React from 'react';
 import './App.css';
-import ErrorBoundary from './ErrorBoundary';
-import MyFriends from './MyFriends';
 
 function App() {
   return (
     <div className='App'>
-      <ErrorBoundary>
-        <MyFriends name='Parmida' />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <MyFriends name='Pantea' />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <MyFriends name='Mahnaz' />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <MyFriends name='Akbar' />
-      </ErrorBoundary>
+      <input type='text' />
     </div>
   );
 }
---
+++ src/RefClass.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class RefClass extends React.Component {}
+
+export default RefClass;
